b064e5055c62ca505e1b037c188ae5bbf17dee4a
jonathan-frazer <jonathanfrazerx146@gmail.com>
2025-02-22 11:08:52 +0530

    Added fsd_hackathon directory

---
+++ fsd_hackathon/nl_to_sql/models.py
@@ -0,0 +1,4 @@
+from db_connection import db
+
+# Create your models here
+login_details = db['t_login']
---
+++ fsd_hackathon/nl_to_sql/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.login_page, name="Login Page"),
+    path('dashboard/', views.dashboard_page, name="Dashboard Page"),
+    path('logout/',views.logout_page,name="Logout Page")
+]
