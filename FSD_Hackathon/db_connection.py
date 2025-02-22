bc69ebe0df2d6ad1f2c77e937ef43ce72b033a61
jonathan-frazer <jonathanfrazerx146@gmail.com>
2025-02-22 11:06:51 +0530

    Delete FSD_Hackathon directory

--- FSD_Hackathon/db_connection.py
+++
@@ -1,5 +0,0 @@
-import pymongo
-
-url = 'mongodb://localhost:27017/'
-client = pymongo.MongoClient(url)
-db = client['hackathon_db']
--- FSD_Hackathon/nl_to_sql/models.py
+++
@@ -1,4 +0,0 @@
-from db_connection import db
-
-# Create your models here
-login_details = db['t_login']
