add38eeb45c5a01c6e6261dddb27a0cec6298929
jonathan-frazer <jonathanfrazerx146@gmail.com>
2025-02-22 09:28:18 +0530

    Initial Infrastructure

--- FSD_Hackathon/FSD_Hackathon/asgi.py
+++ FSD_Hackathon/FSD_Hackathon/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FSD_Hackathon project.
+ASGI config for fsd_hackathon project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FSD_Hackathon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fsd_hackathon.settings')
 
 application = get_asgi_application()
--- FSD_Hackathon/FSD_Hackathon/wsgi.py
+++ FSD_Hackathon/FSD_Hackathon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FSD_Hackathon project.
+WSGI config for fsd_hackathon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FSD_Hackathon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fsd_hackathon.settings')
 
 application = get_wsgi_application()
